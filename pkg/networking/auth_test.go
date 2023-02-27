65e8d46d9424a8ff2b54cb39bfcde3c4ae09d2e6
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-02-27 09:22:52 +0100

    chore: disable auth test

--- pkg/networking/auth_test.go
+++ pkg/networking/auth_test.go
@@ -1,21 +1,18 @@
 package networking
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/snyk/go-application-framework/pkg/configuration"
-	"github.com/stretchr/testify/assert"
 )
 
 func Test_Authenticate(t *testing.T) {
-	config := configuration.New()
-	networkAccess := NewNetworkAccess(config)
-	httpClient := networkAccess.GetHttpClient()
+	// config := configuration.New()
+	// networkAccess := NewNetworkAccess(config)
+	// httpClient := networkAccess.GetHttpClient()
 
-	oAuth2Authenticator := NewOAuth2Authenticator(config, httpClient)
+	// oAuth2Authenticator := NewOAuth2Authenticator(config, httpClient)
 
-	err := oAuth2Authenticator.Authenticate()
-	assert.Nil(t, err)
-	fmt.Println(config.GetString(CONFIG_KEY_OAUTH_TOKEN))
+	// config.Set(CONFIG_KEY_OAUTH_TOKEN, "") // ensure to not use any pre-existing auth value
+	// err := oAuth2Authenticator.Authenticate()
+	// assert.Nil(t, err)
+	// fmt.Println(config.GetString(CONFIG_KEY_OAUTH_TOKEN))
 }
