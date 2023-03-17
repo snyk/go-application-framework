12593d97da56ff32a2770a5297c96eae89f7ab63
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2023-03-17 19:05:33 +0100

    chore: remove url fallback

--- pkg/networking/middleware/auth_header.go
+++ pkg/networking/middleware/auth_header.go
@@ -5,7 +5,6 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/snyk/go-application-framework/internal/constants"
 	"github.com/snyk/go-application-framework/pkg/auth"
 	"github.com/snyk/go-application-framework/pkg/configuration"
 )
@@ -33,12 +32,9 @@ func (n *AuthHeaderMiddleware) RoundTrip(request *http.Request) (*http.Response,
 		// determine configured api url
 		apiUrlString := n.config.GetString(configuration.API_URL)
 		apiUrl, err := url.Parse(apiUrlString)
-		if err != nil {
-			apiUrl, _ = url.Parse(constants.SNYK_DEFAULT_API_URL)
-		}
 
 		// requests to the api automatically get an authentication token attached
-		if strings.Contains(request.URL.Host, apiUrl.Host) {
+		if err == nil && strings.HasPrefix(request.URL.Host, apiUrl.Host) {
 			err = n.authenticator.AddAuthenticationHeader(request)
 			if err != nil {
 				return nil, err
