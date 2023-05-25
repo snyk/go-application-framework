7f1ddad9b96cb7eff31d3c1e1ffe133a105b2f4e
Maximilian Combüchen <max.combuchen@snyk.io>
2023-05-25 11:36:44 +0200

    fix: forward --debug flag in depgraph workflow

--- pkg/local_workflows/depgraph_workflow_test.go
+++ pkg/local_workflows/depgraph_workflow_test.go
@@ -170,6 +170,27 @@ func Test_Depgraph_depgraphWorkflowEntryPoint(t *testing.T) {
 		assert.Equal(t, expected, actual)
 	})
 
+	t.Run("should support 'debug' flag", func(t *testing.T) {
+		// setup
+		config.Set(configuration.DEBUG, true)
+
+		dataIdentifier := workflow.NewTypeIdentifier(WORKFLOWID_DEPGRAPH_WORKFLOW, "depgraph")
+		data := workflow.NewData(dataIdentifier, "application/json", []byte(payload))
+
+		// engine mocks
+		id := workflow.NewWorkflowIdentifier("legacycli")
+		engineMock.EXPECT().InvokeWithConfig(id, config).Return([]workflow.Data{data}, nil).Times(1)
+
+		// execute
+		_, err := depgraphWorkflowEntryPoint(invocationContextMock, []workflow.Data{})
+
+		// assert
+		assert.Nil(t, err)
+
+		commandArgs := config.Get(configuration.RAW_CMD_ARGS)
+		assert.Contains(t, commandArgs, "--debug")
+	})
+
 	t.Run("should support 'all-projects' flag", func(t *testing.T) {
 		// setup
 		config.Set("all-projects", true)
