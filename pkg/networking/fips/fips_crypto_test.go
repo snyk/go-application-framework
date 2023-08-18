01de6708531db2968932dc41ccd83233ab28251b
PeterSchafer <101886095+PeterSchafer@users.noreply.github.com>
2023-08-18 09:15:08 +0200

    chore: add helper to enable fips internally (#86)

--- pkg/networking/fips/fips_crypto_test.go
+++ pkg/networking/fips/fips_crypto_test.go
@@ -2,19 +2,17 @@
 
 package fips
 
+import _ "github.com/snyk/go-application-framework/pkg/networking/fips_enable"
+
 import (
 	"github.com/snyk/go-application-framework/pkg/configuration"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func Test_FIPS_CRYPTO_Validate(t *testing.T) {
+func Test_FIPS_CRYPTO_Auto_Enabled(t *testing.T) {
 	config := configuration.NewInMemory()
-	config.Set(configuration.FIPS_ENABLED, "1")
-	assert.Nil(t, Validate(config))
-
-	config.Set(configuration.FIPS_ENABLED, "0")
-	assert.Nil(t, Validate(config))
+	assert.True(t, config.GetBool(configuration.FIPS_ENABLED))
 }
 
 func Test_FIPS_CRYPTO_IsAvailable(t *testing.T) {
