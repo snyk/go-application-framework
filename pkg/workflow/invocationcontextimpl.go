3b03ebd2488ded46d65027a629766b2fe1815715
Asaf Agami <asaf.agami@snyk.io>
2023-08-21 12:44:19 +0300

    Refactor InvocationContextImpl to unexported type

--- pkg/workflow/invocationcontextimpl.go
+++ pkg/workflow/invocationcontextimpl.go
@@ -19,8 +19,8 @@ func NewInvocationContext(
 	logger zerolog.Logger,
 	analyticsImpl analytics.Analytics,
 	ui ui.UserInterface,
-) InvocationContextImpl {
-	return InvocationContextImpl{
+) InvocationContext {
+	return &invocationContextImpl{
 		WorkflowID:     id,
 		Configuration:  config,
 		WorkflowEngine: engine,
@@ -31,8 +31,8 @@ func NewInvocationContext(
 	}
 }
 
-// InvocationContextImpl is the default implementation of the InvocationContext interface.
-type InvocationContextImpl struct {
+// invocationContextImpl is the default implementation of the InvocationContext interface.
+type invocationContextImpl struct {
 	WorkflowID     Identifier
 	WorkflowEngine Engine
 	Configuration  configuration.Configuration
@@ -43,40 +43,40 @@ type InvocationContextImpl struct {
 }
 
 // GetWorkflowIdentifier returns the identifier of the workflow that is being invoked.
-func (ici *InvocationContextImpl) GetWorkflowIdentifier() Identifier {
+func (ici *invocationContextImpl) GetWorkflowIdentifier() Identifier {
 	return ici.WorkflowID
 }
 
 // GetConfiguration returns the configuration that can be used inside the workflow.
-func (ici *InvocationContextImpl) GetConfiguration() configuration.Configuration {
+func (ici *invocationContextImpl) GetConfiguration() configuration.Configuration {
 	return ici.Configuration
 }
 
 // GetEngine returns the workflow engine that is invoking the workflow.
-func (ici *InvocationContextImpl) GetEngine() Engine {
+func (ici *invocationContextImpl) GetEngine() Engine {
 	return ici.WorkflowEngine
 }
 
 // GetAnalytics returns the analytics instance that is being used by the workflow engine.
-func (ici *InvocationContextImpl) GetAnalytics() analytics.Analytics {
+func (ici *invocationContextImpl) GetAnalytics() analytics.Analytics {
 	return ici.Analytics
 }
 
 // GetNetworkAccess returns the network access instance that is being used by the workflow engine.
-func (ici *InvocationContextImpl) GetNetworkAccess() networking.NetworkAccess {
+func (ici *invocationContextImpl) GetNetworkAccess() networking.NetworkAccess {
 	return ici.networkAccess
 }
 
 // Deprecated: GetLogger returns the logger instance that is being used by the workflow engine.
-func (ici *InvocationContextImpl) GetLogger() *log.Logger {
+func (ici *invocationContextImpl) GetLogger() *log.Logger {
 	return log.New(&utils.ToZeroLogDebug{Logger: ici.logger}, "", 0)
 }
 
 // GetEnhancedLogger returns the logger instance that is being used by the workflow engine.
-func (ici *InvocationContextImpl) GetEnhancedLogger() *zerolog.Logger {
+func (ici *invocationContextImpl) GetEnhancedLogger() *zerolog.Logger {
 	return ici.logger
 }
 
-func (ici *InvocationContextImpl) GetUserInterface() ui.UserInterface {
+func (ici *invocationContextImpl) GetUserInterface() ui.UserInterface {
 	return ici.ui
 }
