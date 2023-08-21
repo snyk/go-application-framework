81a990b73dfbc81099a222d6642ecf94548c2fd5
Asaf Agami <asaf.agami@snyk.io>
2023-08-21 13:20:43 +0300

    Added Input function

--- pkg/ui/userinterface.go
+++ pkg/ui/userinterface.go
@@ -1,9 +1,11 @@
 package ui
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/snyk/go-application-framework/pkg/utils"
@@ -12,17 +14,22 @@ import (
 type UserInterface interface {
 	Output(output string) error
 	OutputError(err error) error
-	ProgressBar() ProgressBar
+	NewProgressBar() ProgressBar
+	Input(prompt string) (string, error)
 }
 
 var red = color.New(color.FgRed)
 
 func NewConsoleUi() UserInterface {
-	return &consoleUi{writer: os.Stdout}
+	return &consoleUi{
+		writer: os.Stdout,
+		reader: bufio.NewReader(os.Stdin),
+	}
 }
 
 type consoleUi struct {
 	writer io.Writer
+	reader *bufio.Reader
 }
 
 func (ui *consoleUi) Output(output string) error {
@@ -33,6 +40,21 @@ func (ui *consoleUi) OutputError(err error) error {
 	return utils.ErrorOf(red.Fprintln(ui.writer, "Error: "+err.Error()))
 }
 
-func (ui *consoleUi) ProgressBar() ProgressBar {
+func (ui *consoleUi) NewProgressBar() ProgressBar {
 	return newProgressBar(ui.writer)
+}
+
+func (ui *consoleUi) Input(prompt string) (string, error) {
+	_, err := fmt.Fprint(ui.writer, prompt, ": ")
+	if err != nil {
+		return "", err
+	}
+
+	input, err := ui.reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	// Trim spaces and newline characters
+	return strings.TrimSpace(input), nil
 }
