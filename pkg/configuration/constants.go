bbb0d2002b01562d54a0b4225c5b4f7064ab7fce
Casey Marshall <cmars@users.noreply.github.com>
2023-11-22 02:33:30 -0600

    feat: snyk_timeout_secs configuration key (#112)

--- pkg/configuration/constants.go
+++ pkg/configuration/constants.go
@@ -26,6 +26,7 @@ const (
 	WORKING_DIRECTORY               string = "internal_working_dir"
 	IS_FEDRAMP                      string = "internal_is_fedramp"
 	UNKNOWN_ARGS                    string = "internal_unknown_arguments" // arguments unknown to the current application but maybe relevant for delegated application calls
+	TIMEOUT                         string = "snyk_timeout_secs"
 
 	// feature flags
 	FF_OAUTH_AUTH_FLOW_ENABLED string = "internal_snyk_oauth_enabled"
