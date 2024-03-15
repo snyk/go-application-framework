bda4a4dcc15c1e5180ed5e52b55ef99e70fb8548
Luke Watts <luke@snyk.io>
2024-03-15 15:36:58 +0100

    fix: introduce flags used by existing snyk code command

--- pkg/local_workflows/code_workflow.go
+++ pkg/local_workflows/code_workflow.go
@@ -12,6 +12,26 @@ const (
 	codeWorkflowName = "code.test"
 )
 
+func GetCodeFlagSet() *pflag.FlagSet {
+	flagSet := pflag.NewFlagSet(codeWorkflowName, pflag.ExitOnError)
+
+	// add flags here
+	flagSet.Bool("sarif", false, "Output in sarif format")
+	flagSet.Bool("json", false, "Output in json format")
+	flagSet.Bool("report", false, "Share results with the Snyk Web UI")
+	flagSet.String("severity-threshold", "", "Minimum severity level to report (low|medium|high)")
+	flagSet.String("sarif-file-output", "", "Save test output in SARIF format directly to the <OUTPUT_FILE_PATH> file, regardless of whether or not you use the --sarif option.")
+	flagSet.String("json-file-output", "", "Save test output in JSON format directly to the <OUTPUT_FILE_PATH> file, regardless of whether or not you use the --json option.")
+	flagSet.String("project-name", "", "The name of the project to test.")
+	flagSet.String("project-id", "", "The unique identifier of the project to test.")
+	flagSet.String("commit-id", "", "The unique identifier of the commit to test.")
+	flagSet.String("target-name", "", "The name of the target to test.")
+	flagSet.String("target-file", "", "The path to the target file to test.")
+	flagSet.String("remote-repo-url", "", "The URL of the remote repository to test.")
+
+	return flagSet
+}
+
 // WORKFLOWID_CODE defines a new workflow identifier
 var WORKFLOWID_CODE workflow.Identifier = workflow.NewWorkflowIdentifier(codeWorkflowName)
 
