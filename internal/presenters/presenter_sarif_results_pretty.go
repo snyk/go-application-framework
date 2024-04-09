d07711c95e31c18f9788c73cb551dddc250fdbdc
Luke Watts <luke@snyk.io>
2024-04-09 09:44:49 +0200

    refactor: move to internal

--- internal/presenters/presenter_sarif_results_pretty.go
+++ internal/presenters/presenter_sarif_results_pretty.go
@@ -1,4 +1,4 @@
-package ui
+package presenters
 
 import (
 	"bytes"
--- internal/presenters/presenter_sarif_results_pretty_test.go
+++ internal/presenters/presenter_sarif_results_pretty_test.go
@@ -1,4 +1,4 @@
-package ui_test
+package presenters_test
 
 import (
 	"encoding/json"
@@ -6,12 +6,12 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/snyk/go-application-framework/pkg/ui"
+	"github.com/snyk/go-application-framework/internal/presenters"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-var testMeta = ui.TestMeta{
+var testMeta = presenters.TestMeta{
 	OrgName:  "test-org",
 	TestPath: "/path/to/project",
 }
@@ -20,7 +20,7 @@ func TestPresenterSarifResultsPretty_NoIssues(t *testing.T) {
 	fd, err := os.Open("testdata/no-issues.json")
 	require.Nil(t, err)
 
-	var input ui.SarifResults
+	var input presenters.SarifResults
 
 	err = json.NewDecoder(fd).Decode(&input)
 	require.Nil(t, err)
@@ -28,7 +28,7 @@ func TestPresenterSarifResultsPretty_NoIssues(t *testing.T) {
 	expected, err := os.ReadFile("testdata/no-issues.txt")
 	require.Nil(t, err)
 
-	result, err := ui.PresenterSarifResultsPretty(input, testMeta)
+	result, err := presenters.PresenterSarifResultsPretty(input, testMeta)
 
 	require.Nil(t, err)
 	assert.Equal(t, string(expected), result)
@@ -38,7 +38,7 @@ func TestPresenterSarifResultsPretty_LowIssues(t *testing.T) {
 	fd, err := os.Open("testdata/3-low-issues.json")
 	require.Nil(t, err)
 
-	var input ui.SarifResults
+	var input presenters.SarifResults
 
 	err = json.NewDecoder(fd).Decode(&input)
 	require.Nil(t, err)
@@ -46,7 +46,7 @@ func TestPresenterSarifResultsPretty_LowIssues(t *testing.T) {
 	expected, err := os.ReadFile("testdata/3-low-issues.txt")
 	require.Nil(t, err)
 
-	result, err := ui.PresenterSarifResultsPretty(input, testMeta)
+	result, err := presenters.PresenterSarifResultsPretty(input, testMeta)
 
 	require.Nil(t, err)
 	assert.Equal(t, strings.Split(string(expected), "\n"), strings.Split(result, "\n"))
@@ -56,7 +56,7 @@ func TestPresenterSarifResultsPretty_MediumHighIssues(t *testing.T) {
 	fd, err := os.Open("testdata/4-high-5-medium.json")
 	require.Nil(t, err)
 
-	var input ui.SarifResults
+	var input presenters.SarifResults
 
 	err = json.NewDecoder(fd).Decode(&input)
 	require.Nil(t, err)
@@ -64,7 +64,7 @@ func TestPresenterSarifResultsPretty_MediumHighIssues(t *testing.T) {
 	expected, err := os.ReadFile("testdata/4-high-5-medium.txt")
 	require.Nil(t, err)
 
-	result, err := ui.PresenterSarifResultsPretty(input, testMeta)
+	result, err := presenters.PresenterSarifResultsPretty(input, testMeta)
 
 	require.Nil(t, err)
 	assert.Equal(t, strings.Split(string(expected), "\n"), strings.Split(result, "\n"))
