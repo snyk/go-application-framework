bf8a08a920bcbe8db1d24c7b947dd440951590c8
Teodora Sandu <81559517+teodora-sandu@users.noreply.github.com>
2024-04-22 16:57:49 +0100

    refactor: use function options pattern (#150)

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 	github.com/rs/zerolog v1.32.0
 	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06
-	github.com/snyk/code-client-go v1.4.2
+	github.com/snyk/code-client-go v1.4.5
 	github.com/snyk/go-httpauth v0.0.0-20231117135515-eb445fea7530
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
--- pkg/local_workflows/code_workflow/code_client_helper.go
+++ pkg/local_workflows/code_workflow/code_client_helper.go
@@ -1,10 +1,7 @@
 package code_workflow
 
 import (
-	"context"
 	"strings"
-
-	"github.com/snyk/code-client-go/observability"
 
 	"github.com/snyk/go-application-framework/pkg/configuration"
 )
@@ -27,37 +24,4 @@ func (c *codeClientConfig) SnykCodeApi() string {
 
 func (c *codeClientConfig) SnykApi() string {
 	return c.localConfiguration.GetString(configuration.API_URL)
-}
-
-type codeClientErrorReporter struct{}
-
-func (c *codeClientErrorReporter) FlushErrorReporting() {}
-func (c *codeClientErrorReporter) CaptureError(err error, options observability.ErrorReporterOptions) bool {
-	return true
-}
-
-type codeClientSpan struct {
-	transactionName string
-	operationName   string
-}
-
-func (c *codeClientSpan) SetTransactionName(name string) { c.transactionName = name }
-func (c *codeClientSpan) StartSpan(ctx context.Context)  {}
-func (c *codeClientSpan) Finish()                        {}
-func (c *codeClientSpan) GetOperation() string           { return c.operationName }
-func (c *codeClientSpan) GetTxName() string              { return c.transactionName }
-func (c *codeClientSpan) GetTraceId() string             { return "" } // TODO: interaction id
-func (c *codeClientSpan) Context() context.Context       { return context.Background() }
-func (c *codeClientSpan) GetDurationMs() int64           { return 0 }
-
-type codeClientInstrumentor struct{}
-
-func (c *codeClientInstrumentor) StartSpan(ctx context.Context, operation string) observability.Span {
-	return &codeClientSpan{operationName: operation}
-}
-func (c *codeClientInstrumentor) NewTransaction(ctx context.Context, txName string, operation string) observability.Span {
-	return &codeClientSpan{operationName: operation, transactionName: txName}
-}
-func (c *codeClientInstrumentor) Finish(span observability.Span) {
-	span.Finish()
 }
