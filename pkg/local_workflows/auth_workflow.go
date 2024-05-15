86824c982460bfef119b95c0eb7230994ec3e578
PeterSchafer <101886095+PeterSchafer@users.noreply.github.com>
2024-05-15 13:43:01 +0200

    fix(auth): automatically switch auth type if an API token is provided (#171)

--- pkg/local_workflows/auth_workflow.go
+++ pkg/local_workflows/auth_workflow.go
@@ -3,7 +3,10 @@ package localworkflows
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
 	"github.com/spf13/pflag"
 
 	"github.com/snyk/go-application-framework/pkg/auth"
@@ -32,14 +35,12 @@ If you can't wait use this url:
 
 // define a new workflow identifier for this workflow
 var WORKFLOWID_AUTH workflow.Identifier = workflow.NewWorkflowIdentifier(workflowNameAuth)
+var ConfigurationNewAuthenticationToken = "internal_new_snyk_token"
 
 // InitAuth initializes the auth workflow before registering it with the engine.
 func InitAuth(engine workflow.Engine) error {
-	if !engine.GetConfiguration().GetBool(configuration.FF_OAUTH_AUTH_FLOW_ENABLED) {
-		return nil // Use legacy CLI for authentication for now, until OAuth is ready
-	}
 	config := pflag.NewFlagSet(workflowNameAuth, pflag.ExitOnError)
-	config.String(authTypeParameter, authTypeOAuth, authTypeDescription)
+	config.String(authTypeParameter, "", authTypeDescription)
 	config.Bool(headlessFlag, false, "Enable headless OAuth authentication")
 	config.String(auth.PARAMETER_CLIENT_SECRET, "", "Client Credential Grant, client secret")
 	config.String(auth.PARAMETER_CLIENT_ID, "", "Client Credential Grant, client id")
@@ -57,37 +58,42 @@ func OpenBrowser(authUrl string) {
 func authEntryPoint(invocationCtx workflow.InvocationContext, _ []workflow.Data) (_ []workflow.Data, err error) {
 	// get necessary objects from invocation context
 	config := invocationCtx.GetConfiguration()
-	logger := invocationCtx.GetLogger()
+	logger := invocationCtx.GetEnhancedLogger()
 	engine := invocationCtx.GetEngine()
 
-	customEndpoint := config.GetString(configuration.API_URL)
-	isOAuthSelected := config.GetString(authTypeParameter) == authTypeOAuth
-	isTokenSelected := config.GetString(authTypeParameter) == authTypeToken
-	var oauthEnabled bool
-	if isOAuthSelected {
-		oauthEnabled = true
-	} else if isTokenSelected {
-		oauthEnabled = false
-	} else {
-		oauthEnabled = auth.IsKnownOAuthEndpoint(customEndpoint)
+	httpClient := invocationCtx.GetNetworkAccess().GetUnauthorizedHttpClient()
+	authenticator := auth.NewOAuth2AuthenticatorWithOpts(
+		config,
+		auth.WithHttpClient(httpClient),
+		auth.WithOpenBrowserFunc(OpenBrowser),
+		auth.WithShutdownServerFunc(auth.ShutdownServer),
+	)
+
+	err = entryPointDI(config, logger, engine, authenticator)
+	return nil, err
+}
+
+func entryPointDI(config configuration.Configuration, logger *zerolog.Logger, engine workflow.Engine, authenticator auth.Authenticator) (err error) {
+	authType := config.GetString(authTypeParameter)
+	if len(authType) == 0 {
+		authType = authTypeOAuth
 	}
 
-	logger.Println("OAuth enabled:", oauthEnabled)
+	// testing if an API token was specified
+	token := config.GetString(ConfigurationNewAuthenticationToken)
+	if _, uuidErr := uuid.Parse(token); uuidErr == nil {
+		authType = authTypeToken
+	}
 
-	if oauthEnabled { // OAUTH flow
+	logger.Printf("Authentication Type: %s", authType)
+
+	if strings.EqualFold(authType, authTypeOAuth) { // OAUTH flow
 		headless := config.GetBool(headlessFlag)
-		logger.Println("Headless:", headless)
+		logger.Printf("Headless: %v", headless)
 
-		httpClient := invocationCtx.GetNetworkAccess().GetUnauthorizedHttpClient()
-		authenticator := auth.NewOAuth2AuthenticatorWithOpts(
-			config,
-			auth.WithHttpClient(httpClient),
-			auth.WithOpenBrowserFunc(OpenBrowser),
-			auth.WithShutdownServerFunc(auth.ShutdownServer),
-		)
 		err = authenticator.Authenticate()
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		fmt.Println(auth.AUTHENTICATED_MESSAGE)
@@ -98,9 +104,9 @@ func authEntryPoint(invocationCtx workflow.InvocationContext, _ []workflow.Data)
 
 		_, legacyCLIError := engine.InvokeWithConfig(workflow.NewWorkflowIdentifier("legacycli"), config)
 		if legacyCLIError != nil {
-			return nil, legacyCLIError
+			return legacyCLIError
 		}
 	}
 
-	return nil, err
+	return err
 }
