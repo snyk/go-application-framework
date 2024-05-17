50a21348fac939020c3eeec2d047876024a42e56
Jason Luong <jason.luong@snyk.io>
2024-05-17 14:05:01 +0100

    feat(analytics): implement GetV2InstrumentationObject

--- pkg/analytics/instrumentation_collector.go
+++ pkg/analytics/instrumentation_collector.go
@@ -1,6 +1,7 @@
 package analytics
 
 import (
+	v20240307 "github.com/snyk/go-application-framework/pkg/analytics/2024-03-07"
 	"time"
 
 	"github.com/snyk/go-application-framework/pkg/local_workflows/json_schemas"
@@ -47,7 +48,7 @@ type instrumentationCollectorImpl struct {
 	testSummary         json_schemas.TestSummary
 	targetId            string // TODO: switch to using purl lib?
 	instrumentationErr  error
-	extension           map[string]string
+	extension           map[string]interface{}
 }
 
 func (ic *instrumentationCollectorImpl) SetUserAgent(ua networking.UserAgentInfo) {
@@ -99,6 +100,88 @@ func (ic *instrumentationCollectorImpl) AddExtension(key string, value string) {
 }
 
 // TODO: this should return a payload(?) that matches the V2 instrumentation schema of the analytics service
-//func GetV2InstrumentationObject(c InstrumentationCollector) json_schemas.AnalyticsV2Request {
-//
-//}
+func (ic *instrumentationCollectorImpl) GetV2InstrumentationObject() v20240307.AnalyticsRequestBody {
+	d := v20240307.AnalyticsData{
+		Type:       ic.instrumentationType,
+		Attributes: ic.getV2Attributes(),
+	}
+
+	return v20240307.AnalyticsRequestBody{
+		Data: d,
+	}
+}
+
+func (ic *instrumentationCollectorImpl) getV2Attributes() v20240307.AnalyticsAttributes {
+	return v20240307.AnalyticsAttributes{
+		Interaction: ic.getV2Interaction(),
+		Runtime:     ic.getV2Runtime(),
+	}
+}
+
+func (ic *instrumentationCollectorImpl) getV2Interaction() v20240307.Interaction {
+	return v20240307.Interaction{
+		Id:          ic.interactionId,
+		Results:     toInteractionResults(&ic.testSummary),
+		Stage:       toInteractionStage(&ic.stage),
+		Target:      v20240307.Target{ic.targetId},
+		TimestampMs: ic.timestamp.UnixMilli(),
+		Type:        ic.instrumentationType,
+		Categories:  &ic.category,
+		Errors:      nil, // TODO: implement this
+	}
+}
+
+// TODO: validate these runtime <> userAgent mappings are correct
+func (ic *instrumentationCollectorImpl) getV2Runtime() *v20240307.Runtime {
+	return &v20240307.Runtime{
+		Application: &v20240307.Application{
+			Name:    ic.userAgent.App,
+			Version: ic.userAgent.AppVersion,
+		},
+		Environment: &v20240307.Environment{
+			Name:    ic.userAgent.IntegrationEnvironment,
+			Version: ic.userAgent.IntegrationEnvironmentVersion,
+		},
+		Integration: &v20240307.Integration{
+			Name:    ic.userAgent.Integration,
+			Version: ic.userAgent.IntegrationVersion,
+		},
+		Performance: &v20240307.Performance{
+			DurationMs: ic.duration.Milliseconds(),
+		},
+		Platform: &v20240307.Platform{
+			Arch: ic.userAgent.Arch,
+			Os:   ic.userAgent.OS,
+		},
+	}
+}
+
+func toInteractionResults(testSummary *json_schemas.TestSummary) *[]map[string]interface{} {
+	r := []map[string]interface{}{}
+	for _, result := range testSummary.Results {
+		r = append(r, map[string]interface{}{
+			"severity": result.Severity,
+			"total":    result.Total,
+			"open":     result.Open,
+			"ignored":  result.Ignored,
+		})
+	}
+	return &r
+}
+
+func toInteractionStage(s *string) *v20240307.InteractionStage {
+	var is v20240307.InteractionStage
+
+	switch stage := s; *stage {
+	case "cicd":
+		is = v20240307.Cicd
+	case "dev":
+		is = v20240307.Dev
+	case "prchecks":
+		is = v20240307.Prchecks
+	default:
+		is = v20240307.Unknown
+	}
+
+	return &is
+}
