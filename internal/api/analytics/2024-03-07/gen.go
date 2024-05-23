0b27291e68923900a20d870aca61e2c4f27fd8fa
Jason Luong <jason.luong@snyk.io>
2024-05-23 08:49:56 +0100

    chore(analytics): remove commented code

--- internal/api/analytics/2024-03-07/gen.go
+++ internal/api/analytics/2024-03-07/gen.go
@@ -1,6 +1,3 @@
 package v20240307
 
 //go:generate oapi-codegen -package=clients -config spec.config.yaml spec.yaml
-
-// We can't pull the spec from the remote as analytics-service is a private repo
-// //go:generate bash -c "mkdir -p 2024-03-07 && curl -s https://raw.githubusercontent.com/snyk/analytics-service/main/internal/hidden/resources/analytics/2024-03-07/spec.yaml | oapi-codegen -package=v20240307 -generate=types,client,spec -o=v20240307/client-gen.go /dev/stdin"
