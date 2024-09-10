40654925706bc1e5d9d0475686712d408c8c570e
Luke Watts <luke@snyk.io>
2024-09-10 14:22:07 +0200

    test: validate sarif-juice-shop

--- internal/cue_utils/transformer_test.go
+++ internal/cue_utils/transformer_test.go
@@ -5,30 +5,13 @@ import (
 	"os"
 	"testing"
 
+	"cuelang.org/go/cue/ast"
 	"cuelang.org/go/cue/cuecontext"
 	cuejson "cuelang.org/go/pkg/encoding/json"
 )
 
 func TestNewTransformer_ValidTransformToTestApiFromCliTestManaged(t *testing.T) {
 	ctx := cuecontext.New()
-
-	//
-	jsonFile, err := os.Open("./testdata/inputs/cli-json-test-npm.json")
-	if err != nil {
-		t.Errorf("Failed to load json")
-	}
-	defer func(jsonFile *os.File) {
-		jsonErr := jsonFile.Close()
-		if jsonErr != nil {
-			t.Errorf("Failed to close json")
-		}
-	}(jsonFile)
-	byteValue, _ := io.ReadAll(jsonFile)
-	input, errUnJson := cuejson.Unmarshal(byteValue)
-
-	if errUnJson != nil {
-		t.Errorf("Unexpected error parsing JSON: %v", err)
-	}
 
 	transformer, err := NewTransformer(ctx, ToTestApiFromCliTestManaged)
 	if err != nil {
@@ -39,6 +22,7 @@ func TestNewTransformer_ValidTransformToTestApiFromCliTestManaged(t *testing.T) 
 		t.Error("Expected a non-nil transformer")
 	}
 
+	input := loadJsonFile(t, "cli-json-test-npm.json")
 	_, applyError := transformer.Apply(input)
 	if applyError == nil {
 		t.Fatal(err)
@@ -54,6 +38,12 @@ func TestNewTransformer_ValidTransformToTestApiFromSarif(t *testing.T) {
 
 	if transformer == nil {
 		t.Error("Expected a non-nil transformer")
+	}
+
+	input := loadJsonFile(t, "sarif-juice-shop.json")
+	_, applyError := transformer.Apply(input)
+	if applyError == nil {
+		t.Fatal(err)
 	}
 }
 
@@ -76,4 +66,30 @@ func TestNewTransformer_InvalidTransform(t *testing.T) {
 	if err == nil {
 		t.Error("Expected an error for invalid transform")
 	}
+}
+
+func loadJsonFile(t *testing.T, filename string) ast.Expr {
+	t.Helper()
+
+	jsonFile, err := os.Open("./testdata/inputs/" + filename)
+	if err != nil {
+		t.Errorf("Failed to load json")
+	}
+	defer func(jsonFile *os.File) {
+		jsonErr := jsonFile.Close()
+		if jsonErr != nil {
+			t.Errorf("Failed to close json")
+		}
+	}(jsonFile)
+	byteValue, jsonReadAllErr := io.ReadAll(jsonFile)
+	if jsonReadAllErr != nil {
+		t.Errorf("Unexpected error reading JSON file %v", jsonReadAllErr)
+	}
+
+	input, errUnJson := cuejson.Unmarshal(byteValue)
+
+	if errUnJson != nil {
+		t.Errorf("Unexpected error parsing JSON: %v", err)
+	}
+	return input
 }
