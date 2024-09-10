8e8a623440b4d6209f662913d826affc59488c2f
Luke Watts <luke@snyk.io>
2024-09-10 14:10:33 +0200

    feat: add import step for types
    Hardcoded package name
    https://github.com/snyk/dragonfly/blob/main/schema/lib/gotypes/index.ts#L99-L100

--- scripts/pull-down-test-api-spec.sh
+++ scripts/pull-down-test-api-spec.sh
@@ -11,10 +11,17 @@ npm ci
 # Trigger build of dragonfly project
 npm run build
 
+# Return to project directory
 cd -
 
 # Vendor OpenAPI build artefacts for use in cue
 cp -r $API_SPEC_PATH/tsp-output/@typespec/openapi3/ ./internal/cue_utils/source/openapi
+# Import types for go
+cp $API_SPEC_PATH/tsp-output/go/typespec_gen.go ./internal/dragonfly/dragonfly.go
+
+# Rename imported go types
+# In the future the package name will be configurable
+sed -i "" 's/package\ presentation/package\ dragonfly/g' internal/dragonfly/dragonfly.go
 
 cd ./internal/cue_utils
 go generate
