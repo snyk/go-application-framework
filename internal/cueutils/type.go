51b1f786cc506354a1f99b329f0e573a84848b82
Luke Watts <luke@snyk.io>
2024-09-12 16:53:09 +0200

    fix: explicitly declare json prop

--- internal/cueutils/type.go
+++ internal/cueutils/type.go
@@ -3,7 +3,7 @@ package cueutils
 import "github.com/snyk/go-application-framework/internal/restapimodels"
 
 type LocalFinding struct {
-	Findings []restapimodels.FindingResource
-	Summary  restapimodels.FindingsSummary
-	Outcome  restapimodels.TestOutcome
+	Findings []restapimodels.FindingResource `json:"findings"`
+	Summary  restapimodels.FindingsSummary   `json:"summary"`
+	Outcome  restapimodels.TestOutcome       `json:"outcome"`
 }
