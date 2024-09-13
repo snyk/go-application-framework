6793434c3c9492ae7ab94b980193a35c388805b6
Luke Watts <luke@snyk.io>
2024-09-13 11:29:14 +0200

    chore: flag for refactoring

--- internal/cueutils/type.go
+++ internal/cueutils/type.go
@@ -2,6 +2,7 @@ package cueutils
 
 import "github.com/snyk/go-application-framework/internal/restapimodels"
 
+// TODO: This schema should be imported from Dragonfly
 type LocalFinding struct {
 	Findings []restapimodels.FindingResource `json:"findings"`
 	Summary  restapimodels.FindingsSummary   `json:"summary"`
