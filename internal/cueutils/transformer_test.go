cb7661f23daaa00fd4caca9c2687b6a68551edd1
Luke Watts <luke@snyk.io>
2024-09-24 16:24:06 +0100

    feat: add transformation workflow

--- internal/cueutils/transformer_test.go
+++ internal/cueutils/transformer_test.go
@@ -11,7 +11,6 @@ import (
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/encoding/gocode/gocodec"
 	cuejson "cuelang.org/go/pkg/encoding/json"
-	"github.com/snyk/go-application-framework/pkg/local_workflows/local_models"
 	"github.com/stretchr/testify/assert"
 )
 
