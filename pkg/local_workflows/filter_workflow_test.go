a1291f42e64c18780f2a7d36ff865430b03caf90
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-11-04 18:35:38 +0100

    test: increase test coverage

--- pkg/local_workflows/filter_workflow_test.go
+++ pkg/local_workflows/filter_workflow_test.go
@@ -6,12 +6,13 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+
 	"github.com/snyk/go-application-framework/pkg/configuration"
 	"github.com/snyk/go-application-framework/pkg/local_workflows/content_type"
 	"github.com/snyk/go-application-framework/pkg/local_workflows/local_models"
 	"github.com/snyk/go-application-framework/pkg/mocks"
 	"github.com/snyk/go-application-framework/pkg/workflow"
-	"github.com/stretchr/testify/assert"
 )
 
 func setupMockFilterContext(t *testing.T, severityThreshold string) *mocks.MockInvocationContext {
@@ -33,12 +34,63 @@ func setupMockFilterContext(t *testing.T, severityThreshold string) *mocks.MockI
 	return invocationContextMock
 }
 
+func getRandomWorkflowData() workflow.Data {
+	return workflow.NewData(
+		workflow.NewTypeIdentifier(WORKFLOWID_FILTER_FINDINGS, "randomtype"),
+		"application/random",
+		[]byte{},
+	)
+}
+
+func getFindingsInputData(t *testing.T) workflow.Data {
+	t.Helper()
+	sarifBytes := loadJsonFile(t, "sarif-juice-shop.json")
+	summaryBytes := loadJsonFile(t, "juice-shop-summary.json")
+
+	findings, err := TransformToLocalFindingModel(sarifBytes, summaryBytes)
+	assert.NoError(t, err)
+
+	findingsBytes, err := json.Marshal(findings)
+	assert.NoError(t, err)
+	return workflow.NewData(
+		workflow.NewTypeIdentifier(WORKFLOWID_FILTER_FINDINGS, FilterFindingsWorkflowName),
+		content_type.LOCAL_FINDING_MODEL,
+		findingsBytes,
+	)
+}
+
+func getFindingsInputDataBroken() workflow.Data {
+	return workflow.NewData(
+		workflow.NewTypeIdentifier(WORKFLOWID_FILTER_FINDINGS, FilterFindingsWorkflowName),
+		content_type.LOCAL_FINDING_MODEL,
+		[]byte("not json"),
+	)
+}
+
+func TestFilterWorkflowRegistration(t *testing.T) {
+	config := configuration.NewWithOpts()
+	engine := workflow.NewWorkFlowEngine(config)
+	err := InitFilterFindingsWorkflow(engine)
+	assert.NoError(t, err)
+	entry, ok := engine.GetWorkflow(WORKFLOWID_FILTER_FINDINGS)
+	assert.True(t, ok)
+	assert.NotNil(t, entry)
+}
+
 func TestFilterFindingsEntryPoint(t *testing.T) {
 	t.Run("with invalid payload type", func(t *testing.T) {
 		ctx := setupMockFilterContext(t, "low")
 		input := []workflow.Data{workflow.NewData(
 			workflow.NewTypeIdentifier(WORKFLOWID_FILTER_FINDINGS, FilterFindingsWorkflowName),
 			content_type.LOCAL_FINDING_MODEL, nil)}
+		output, err := filterFindingsEntryPoint(ctx, input)
+		assert.NoError(t, err)
+		assert.Equal(t, input[0], output[0])
+	})
+
+	t.Run("with non json payload type", func(t *testing.T) {
+		ctx := setupMockFilterContext(t, "low")
+		input := []workflow.Data{getFindingsInputDataBroken()}
 		output, err := filterFindingsEntryPoint(ctx, input)
 		assert.NoError(t, err)
 		assert.Equal(t, input[0], output[0])
@@ -59,22 +111,16 @@ func TestFilterFindingsEntryPoint(t *testing.T) {
 		assert.Equal(t, input, output)
 	})
 
-	t.Run("filters findings with treshold set to high", func(t *testing.T) {
-		ctx := setupMockFilterContext(t, "high")
-		sarifBytes := loadJsonFile(t, "sarif-juice-shop.json")
-		summaryBytes := loadJsonFile(t, "juice-shop-summary.json")
-		findings, err := TransformToLocalFindingModel(sarifBytes, summaryBytes)
-		assert.NoError(t, err)
-		findingsBytes, err := json.Marshal(findings)
-		assert.NoError(t, err)
-		input := []workflow.Data{workflow.NewData(
-			workflow.NewTypeIdentifier(WORKFLOWID_FILTER_FINDINGS, FilterFindingsWorkflowName),
-			content_type.LOCAL_FINDING_MODEL,
-			findingsBytes,
-		)}
+	t.Run("filters findings with threshold set to high", func(t *testing.T) {
+		ctx := setupMockFilterContext(t, "hIgH")
+
+		randomInputData := getRandomWorkflowData()
+		findingsData := getFindingsInputData(t)
+
+		input := []workflow.Data{findingsData, randomInputData}
 		output, err := filterFindingsEntryPoint(ctx, input)
 		assert.NoError(t, err)
-		assert.Equal(t, 1, len(output))
+		assert.Equal(t, len(input), len(output))
 		var filteredFindings local_models.LocalFinding
 		err = json.Unmarshal(output[0].GetPayload().([]byte), &filteredFindings)
 
@@ -83,5 +129,18 @@ func TestFilterFindingsEntryPoint(t *testing.T) {
 			severity := string(finding.Attributes.Rating.Severity.Value)
 			assert.True(t, severity == "high" || severity == "critical", "Unexpected severity: %s", severity)
 		}
+
+		assert.Equal(t, randomInputData, output[1])
+	})
+
+	t.Run("pass input data through when severity threshold is not set", func(t *testing.T) {
+		ctx := setupMockFilterContext(t, "")
+		randomInputData := getRandomWorkflowData()
+		randomInputData2 := getRandomWorkflowData()
+		findingsData := getFindingsInputData(t)
+		input := []workflow.Data{randomInputData, findingsData, randomInputData2}
+		output, err := filterFindingsEntryPoint(ctx, input)
+		assert.NoError(t, err)
+		assert.Equal(t, input, output)
 	})
 }
