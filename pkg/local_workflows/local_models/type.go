8193ab6908f87fea850d05f7a45c8678b8b5ad9a
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-11-05 16:28:33 +0100

    chore: add type aliases

--- pkg/local_workflows/local_models/type.go
+++ pkg/local_workflows/local_models/type.go
@@ -2,6 +2,9 @@ package local_models
 
 import "github.com/snyk/go-application-framework/pkg/local_workflows/json_schemas"
 
+type FindingResource TypesFindingResource
+type TestOutcome TypesTestOutcome
+
 // TODO: This schema should be imported from Dragonfly
 type LocalFinding struct {
 	Findings []FindingResource `json:"findings"`
