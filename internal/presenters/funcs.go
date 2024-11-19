d37414b4916ace8f61061b5766bcacce5cbdbf53
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-11-19 09:07:32 +0100

    chore: remap fingerprints

--- internal/presenters/funcs.go
+++ internal/presenters/funcs.go
@@ -198,6 +198,9 @@ func getDefaultTemplateFuncMap(config configuration.Configuration, ri runtimeinf
 	defaultMap["getQuotedString"] = func(input string) string {
 		return strconv.Quote(input)
 	}
+	defaultMap["replaceString"] = func(str string, old string, new string) string {
+		return strings.ReplaceAll(str, old, new)
+	}
 
 	return defaultMap
 }
