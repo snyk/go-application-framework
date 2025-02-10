1f7ea0f7ebe6e18b3ed916226438e591efe52fd1
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2025-02-10 21:38:39 +0100

    chore: minor refactoring

--- pkg/local_workflows/output_workflow/constants.go
+++ pkg/local_workflows/output_workflow/constants.go
@@ -6,6 +6,6 @@ const (
 	OUTPUT_CONFIG_KEY_SARIF        = "sarif"
 	OUTPUT_CONFIG_KEY_SARIF_FILE   = "sarif-file-output"
 	OUTPUT_CONFIG_TEMPLATE_FILE    = "internal_template_file"
-	DEFAULT_WRITER                 = "default"
 	OUTPUT_CONFIG_WRITE_EMPTY_FILE = "internal_write_empty_file"
+	DEFAULT_WRITER                 = "default"
 )
