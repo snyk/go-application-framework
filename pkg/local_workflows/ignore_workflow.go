f23c439b8bf03b4711fd268b23a2855a3b41b7da
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-04-07 03:22:20 +0200

    feat: create payload and call policy API

--- pkg/local_workflows/ignore_workflow.go
+++ pkg/local_workflows/ignore_workflow.go
@@ -1,15 +1,21 @@
 package localworkflows
 
 import (
-	"github.com/go-git/go-git/v5"
-	"github.com/spf13/pflag"
-
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"strings"
 	"time"
 
+	"github.com/rs/zerolog"
+	"github.com/spf13/pflag"
+
+	policyApi "github.com/snyk/go-application-framework/internal/api/policy/2024-10-15"
 	"github.com/snyk/go-application-framework/pkg/configuration"
 	"github.com/snyk/go-application-framework/pkg/local_workflows/code_workflow"
+	"github.com/snyk/go-application-framework/pkg/utils/git"
 	"github.com/snyk/go-application-framework/pkg/workflow"
 )
 
@@ -38,7 +44,13 @@ const (
 
 	interactiveKey    = "interactive"
 	enrichResponseKey = "enrich_response"
+
+	policyAPIVersion = "2024-10-15"
 )
+
+// TODOS:
+// Add ignore enum check
+// Add httpclient and somehow call it
 
 var WORKFLOWID_IGNORE_CREATE workflow.Identifier = workflow.NewWorkflowIdentifier(ignoreCreateWorkflowName)
 var WORKFLOWID_IGNORE_EDIT workflow.Identifier = workflow.NewWorkflowIdentifier(ignoreEditWorkflowName)
@@ -69,7 +81,7 @@ func InitIgnoreWorkflows(engine workflow.Engine) error {
 	_, err = engine.Register(WORKFLOWID_IGNORE_EDIT, workflow.ConfigurationOptionsFromFlagset(editFlagset), ignoreEditWorkflowEntryPoint)
 
 	deleteFlagSet := pflag.NewFlagSet(ignoreDeletWorkflowName, pflag.ExitOnError)
-	editFlagset.String(ignoreIdKey, "", ignoreIdDescription)
+	deleteFlagSet.String(ignoreIdKey, "", ignoreIdDescription)
 	// If set to false, no response will be returned
 	deleteFlagSet.Bool(enrichResponseKey, false, "")
 	deleteFlagSet.Bool(interactiveKey, true, "")
@@ -78,17 +90,12 @@ func InitIgnoreWorkflows(engine workflow.Engine) error {
 	return err
 }
 
-func getIgnoreRequestDetailsStructure(expire time.Time, userName string) string {
+func getIgnoreRequestDetailsStructure(expire time.Time, userName string, ignoreType string) string {
 	expireDisplayText := "Does not expire"
 	if !expire.IsZero() {
 		expireDisplayText = expire.Format(time.DateOnly)
-
 	}
-	return fmt.Sprintf("  Requested on:  2024-08-10\n  Requested by:  %s\n  Expiration:    %s\n  Ignore type:   false-positive", userName, expireDisplayText)
-}
-
-func sendRequest() {
-
+	return fmt.Sprintf("  Requested on:  2024-08-10\n  Requested by:  %s\n  Expiration:    %s\n  Ignore type:   %s", userName, expireDisplayText, ignoreType)
 }
 
 func ignoreEditWorkflowEntryPoint(_ workflow.InvocationContext, _ []workflow.Data) (output []workflow.Data, err error) {
@@ -102,57 +109,137 @@ func ignoreCreateWorkflowEntryPoint(invocationCtx workflow.InvocationContext, _ 
 	logger := invocationCtx.GetEnhancedLogger()
 	userInterface := invocationCtx.GetUserInterface()
 	config := invocationCtx.GetConfiguration()
-	interactive := config.GetBool(interactiveKey)
+	id := invocationCtx.GetWorkflowIdentifier()
 
+	interactive := config.GetBool(interactiveKey)
 	if interactive {
 		// add interactive default function
 		addDefaultCreateInteractiveValues(invocationCtx)
 	}
 
-	repoUrl, err := config.GetWithError(code_workflow.ConfigurationRemoteRepoUrlFlagname)
-	if err != nil {
-		return nil, err
-	}
-
-	_ = userInterface.Output(repoUrl)
-
-	// get user
-	config.Set(configuration.FLAG_EXPERIMENTAL, true)
-
 	userName, err := getUserName(invocationCtx)
 	if err != nil {
+		return nil, fmt.Errorf("user is not authenticated: %w", err)
+	}
+
+	findingsId, err := config.GetStringWithError(findingsIdKey)
+	if err != nil {
 		return nil, err
 	}
+	_ = userInterface.Output(findingsId)
+
+	ignoreType, err := config.GetStringWithError(ignoreTypeKey)
+	if err != nil {
+		return nil, err
+	}
+	_ = userInterface.Output(ignoreType)
+
+	repoUrl, err := config.GetStringWithError(code_workflow.ConfigurationRemoteRepoUrlFlagname)
+	if err != nil {
+		return nil, err
+	}
+	_ = userInterface.Output(repoUrl)
 
 	// read expiry time
 	var expire time.Time
-	if config.IsSet(expirationKey) {
-		expire, err = time.Parse(time.DateOnly, config.GetString(expirationKey))
-		if err != nil {
-			return output, err
-		}
+	if expireStr, getConfigErr := config.GetStringWithError(expirationKey); getConfigErr == nil {
+		// TODO: if we can't parse date should it be treated as non-expire?
+		expire, _ = time.Parse(time.DateOnly, expireStr)
+	} else {
+		return nil, getConfigErr
+	}
+
+	_ = userInterface.Output(expire.Format(time.DateOnly))
+
+	reason, err := config.GetStringWithError(reasonKey)
+	if err != nil {
+		return nil, err
 	}
 
 	if interactive {
 		_ = userInterface.Output("You are about to ignore the following issue:\n👉🏼 Make sure the code containing the issue is committed, and pushed to a remote origin, so the approvers are able to analyze it.\n")
-		_ = userInterface.Output(getIgnoreRequestDetailsStructure(expire, userName))
+		_ = userInterface.Output(getIgnoreRequestDetailsStructure(expire, userName, ignoreType))
+		yesNoString, inputError := userInterface.Input("\nAdd a reason for ignoring this issue [Y/N]?")
+		if inputError != nil {
+			return nil, inputError
+		}
+		if strings.ToLower(yesNoString) != "y" && strings.ToLower(yesNoString) != "yes" {
+			return nil, fmt.Errorf("operation canceled by user")
+		}
 	}
 
-	reason := ""
-	if !config.IsSet(reasonKey) && config.GetBool(enterReasonKey) {
-		reason = config.GetString(reasonKey)
+	url := fmt.Sprintf("%s/rest/orgs/%s/policies?version=%s", config.GetString(configuration.API_URL), config.Get(configuration.ORGANIZATION), policyAPIVersion)
+	branchName, _ := git.BranchNameFromDir(config.GetString(configuration.INPUT_DIRECTORY))
+	payload := createPayload(repoUrl, branchName, expire, ignoreType, reason, findingsId)
+	response, err := createPolicy(invocationCtx, payload, url)
+
+	if err != nil {
+		return nil, err
 	}
 
-	logger.Printf("Reason: %s\n", reason)
-	logger.Printf("Expires: %s\n", expire)
-
-	sendRequest()
+	logger.Printf(string(response.Attributes.Review))
 
 	if interactive {
 		_ = userInterface.Output("\nYour ignore request has been submitted for approval.")
 	}
 
+	if config.GetBool(enrichResponseKey) {
+		data, workFlowDataErr := createCodeWorkflowData(
+			workflow.NewTypeIdentifier(id, ignoreCreateWorkflowName),
+			config,
+			response,
+			logger)
+		if workFlowDataErr != nil {
+			return nil, workFlowDataErr
+		}
+		output = append(output, data)
+	}
+
 	return output, err
+}
+
+func createCodeWorkflowData(id workflow.Identifier, config configuration.Configuration, obj any, logger *zerolog.Logger) (workflow.Data, error) {
+	marshalledObj, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	data := workflow.NewData(
+		id,
+		"application/json",
+		marshalledObj,
+		workflow.WithConfiguration(config),
+		workflow.WithLogger(logger),
+	)
+
+	return data, nil
+}
+
+func createPayload(repoUrl string, branchName string, expire time.Time, ignoreType string, reason string, findingsId string) policyApi.CreatePolicyPayload {
+	meta := policyApi.Meta{}
+	meta["repo_url"] = repoUrl
+	meta["branch_name"] = branchName
+
+	var payload policyApi.CreatePolicyPayload
+	payload.Data.Meta = &meta
+	payload.Data.Attributes.Action.Data = policyApi.PolicyActionIgnoreData{
+		Expires:    &expire,
+		IgnoreType: policyApi.PolicyActionIgnoreDataIgnoreType(ignoreType),
+		Reason:     &reason,
+	}
+	payload.Data.Attributes.ActionType = policyApi.PolicyAttributesActionTypeIgnore
+	payload.Data.Attributes.ConditionsGroup = policyApi.PolicyConditionsGroup{
+		Conditions: []policyApi.PolicyCondition{
+			{
+				Field:    policyApi.Snykassetfindingv1,
+				Operator: policyApi.Includes,
+				Value:    findingsId,
+			},
+		},
+		LogicalOperator: policyApi.And,
+	}
+	payload.Data.Type = policyApi.CreatePolicyPayloadDataTypePolicy
+	return payload
 }
 
 func addDefaultCreateInteractiveValues(invocationCtx workflow.InvocationContext) {
@@ -167,10 +254,22 @@ func addDefaultCreateInteractiveValues(invocationCtx workflow.InvocationContext)
 	})
 
 	config.AddDefaultValue(ignoreTypeKey, func(existingValue interface{}) (interface{}, error) {
+		invalidIgnoreTypeErr := fmt.Errorf("invalid ignore type, valid ignore types are: %s, %s, %s",
+			policyApi.NotVulnerable, policyApi.TemporaryIgnore, policyApi.WontFix)
 		if existingValue != nil && existingValue != "" {
+			if !validIgnoreType(existingValue.(string)) {
+				return "", invalidIgnoreTypeErr
+			}
 			return existingValue, nil
 		}
-		return userInterface.Input(ignoreTypeDescription)
+		ignoreType, err := userInterface.Input(ignoreTypeDescription)
+		if err != nil {
+			return "", err
+		}
+		if !validIgnoreType(ignoreType) {
+			return "", invalidIgnoreTypeErr
+		}
+		return ignoreType, nil
 	})
 
 	config.AddDefaultValue(reasonKey, func(existingValue interface{}) (interface{}, error) {
@@ -193,25 +292,10 @@ func addDefaultCreateInteractiveValues(invocationCtx workflow.InvocationContext)
 		}
 
 		getRepoUrlFromRepo := func() (string, error) {
-			repo, err := git.PlainOpenWithOptions(config.GetString(configuration.INPUT_DIRECTORY), &git.PlainOpenOptions{
-				DetectDotGit: true,
-			})
-
+			repoUrl, err := git.RepoUrlFromDir(config.GetString(configuration.INPUT_DIRECTORY))
 			if err != nil {
 				return "", err
 			}
-
-			remote, err := repo.Remote("origin")
-			if err != nil {
-				return "", err
-			}
-
-			// based on the docs, the first URL is being used to fetch, so this is the one we use
-			remoteConfig := remote.Config()
-			if remoteConfig == nil || len(remoteConfig.URLs) == 0 || remoteConfig.URLs[0] == "" {
-				return "", fmt.Errorf("no remote url found")
-			}
-			repoUrl := remoteConfig.URLs[0]
 			return repoUrl, nil
 		}
 
@@ -222,27 +306,18 @@ func addDefaultCreateInteractiveValues(invocationCtx workflow.InvocationContext)
 
 		return repoUrl, nil
 	})
+}
 
-	config.AddDefaultValue(reasonKey, func(existingValue interface{}) (interface{}, error) {
-		if existingValue != nil && existingValue != "" {
-			return existingValue, nil
-		}
-		yesNoString, inputError := userInterface.Input("\nAdd a reason for ignoring this issue [Y/N]?")
-		if inputError != nil {
-			return false, inputError
-		}
-
-		switch strings.ToLower(yesNoString) {
-		case "y", "yes":
-			return true, nil
-		default:
-			return false, nil
-		}
-	})
+func validIgnoreType(ignoreType string) bool {
+	ignoreTypeMapped := policyApi.PolicyActionIgnoreDataIgnoreType(ignoreType)
+	return ignoreTypeMapped == policyApi.TemporaryIgnore || ignoreTypeMapped == policyApi.WontFix || ignoreTypeMapped == policyApi.NotVulnerable
 }
 
 func getUserName(invocationCtx workflow.InvocationContext) (string, error) {
-	data, err := invocationCtx.GetEngine().InvokeWithConfig(WORKFLOWID_WHOAMI, invocationCtx.GetConfiguration())
+	config := invocationCtx.GetConfiguration().Clone()
+	config.Set(configuration.FLAG_EXPERIMENTAL, true)
+
+	data, err := invocationCtx.GetEngine().InvokeWithConfig(WORKFLOWID_WHOAMI, config)
 	if err != nil {
 		return "", err
 	}
@@ -257,4 +332,40 @@ func getUserName(invocationCtx workflow.InvocationContext) (string, error) {
 	} else {
 		return "", noAuthErr
 	}
+}
+
+func createPolicy(invocationCtx workflow.InvocationContext, input policyApi.CreatePolicyPayload, url string) (*policyApi.PolicyResponse, error) {
+	// Create a request
+	requestBody, err := json.Marshal(input)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/vnd.api+json")
+
+	// Send the request
+	resp, err := invocationCtx.GetNetworkAccess().GetHttpClient().Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var responseWrapper struct {
+		Data policyApi.PolicyResponse `json:"data"`
+	}
+
+	err = json.Unmarshal(responseBody, &responseWrapper)
+
+	if err != nil || resp.StatusCode != http.StatusCreated {
+		return nil, fmt.Errorf("error sending request: Status %s StatusCode %d", resp.Status, resp.StatusCode)
+	}
+
+	return &responseWrapper.Data, nil
 }
---
+++ pkg/local_workflows/ignore_workflow_test.go
@@ -0,0 +1,246 @@
+package localworkflows
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+
+	policyApi "github.com/snyk/go-application-framework/internal/api/policy/2024-10-15"
+	"github.com/snyk/go-application-framework/pkg/configuration"
+	"github.com/snyk/go-application-framework/pkg/mocks"
+	"github.com/snyk/go-application-framework/pkg/workflow"
+)
+
+const (
+	policyResponseJSON = `{
+	"data": {
+		"type": "policy",
+		"id": "12345678-1234-1234-1234-123456789012",
+		"attributes": {
+			"name": "Test Policy",
+			"created_at": "2024-01-01T00:00:00Z",
+			"updated_at": "2024-01-01T00:00:00Z"
+		}
+	}
+}`
+	testRepoUrl    = "https://github.com/snyk/snyk-goof"
+	testBranchName = "main"
+	expectedUser   = "user@domain.com"
+)
+
+// Setup mock context for testing
+func setupMockIgnoreContext(t *testing.T, payload string, statusCode int, mockClient bool) (workflow.Engine, workflow.InvocationContext) {
+	t.Helper()
+
+	// setup
+	logger := zerolog.Logger{}
+	config := configuration.New()
+
+	// setup mocks
+	ctrl := gomock.NewController(t)
+	networkAccessMock := mocks.NewMockNetworkAccess(ctrl)
+	invocationContextMock := mocks.NewMockInvocationContext(ctrl)
+	mockUserInterface := mocks.NewMockUserInterface(ctrl)
+	mockEngine := mocks.NewMockEngine(ctrl)
+
+	mockUserInterface.EXPECT().Output(gomock.Any()).Return(nil).AnyTimes()
+	mockUserInterface.EXPECT().Input(gomock.Any()).Return("", nil).AnyTimes()
+
+	var httpClient = http.DefaultClient
+
+	if mockClient {
+		httpClient = newTestClient(func(req *http.Request) *http.Response {
+			return &http.Response{
+				StatusCode: statusCode,
+				// Send response to be tested
+				Body: io.NopCloser(bytes.NewBufferString(payload)),
+				// Must be set to non-nil value or it panics
+				Header: make(http.Header),
+			}
+		})
+	}
+
+	// setup invocation context
+	invocationContextMock.EXPECT().GetConfiguration().Return(config).AnyTimes()
+	invocationContextMock.EXPECT().GetEnhancedLogger().Return(&logger).AnyTimes()
+	invocationContextMock.EXPECT().GetNetworkAccess().Return(networkAccessMock).AnyTimes()
+	invocationContextMock.EXPECT().GetEngine().Return(mockEngine).AnyTimes()
+	invocationContextMock.EXPECT().GetUserInterface().Return(mockUserInterface).AnyTimes()
+	invocationContextMock.EXPECT().GetWorkflowIdentifier().Return(workflow.NewWorkflowIdentifier(ignoreCreateWorkflowName)).AnyTimes()
+	networkAccessMock.EXPECT().GetHttpClient().Return(httpClient).AnyTimes()
+	mockData := []workflow.Data{workflow.NewData(
+		workflow.NewTypeIdentifier(WORKFLOWID_WHOAMI, "whoami"),
+		"text/plain",
+		expectedUser,
+		workflow.WithLogger(&logger),
+	)}
+	mockEngine.EXPECT().InvokeWithConfig(WORKFLOWID_WHOAMI, gomock.Any()).Return(mockData, nil).AnyTimes()
+
+	return mockEngine, invocationContextMock
+}
+
+func Test_getIgnoreRequestDetailsStructure(t *testing.T) {
+	expireDate, err := time.Parse(time.DateOnly, "2025-01-01")
+	assert.NoError(t, err)
+	userName := "test-user"
+	ignoreType := "wont-fix"
+
+	result := getIgnoreRequestDetailsStructure(expireDate, userName, ignoreType)
+
+	assert.Contains(t, result, "2025-01-01")
+	assert.Contains(t, result, userName)
+	assert.Contains(t, result, ignoreType)
+}
+
+func Test_validIgnoreType(t *testing.T) {
+	validTypes := []string{
+		string(policyApi.TemporaryIgnore),
+		string(policyApi.WontFix),
+		string(policyApi.NotVulnerable),
+	}
+
+	for _, validType := range validTypes {
+		t.Run("valid: "+validType, func(t *testing.T) {
+			assert.True(t, validIgnoreType(validType), "Should be a valid ignore type")
+		})
+	}
+
+	invalidTypes := []string{
+		"invalid",
+		"",
+		"permanently-ignore",
+	}
+
+	for _, invalidType := range invalidTypes {
+		t.Run("invalid: "+invalidType, func(t *testing.T) {
+			assert.False(t, validIgnoreType(invalidType), "Should be an invalid ignore type")
+		})
+	}
+}
+
+func Test_createPolicy(t *testing.T) {
+	t.Run("successful creation", func(t *testing.T) {
+		_, invocationContext := setupMockIgnoreContext(t, policyResponseJSON, http.StatusCreated, true)
+
+		var input policyApi.CreatePolicyPayload
+		input.Data.Type = policyApi.CreatePolicyPayloadDataTypePolicy
+
+		result, err := createPolicy(invocationContext, input, testRepoUrl)
+
+		assert.NoError(t, err, "Should not return an error")
+		assert.NotNil(t, result, "Should return a policy response")
+		expectedUuid, _ := uuid.Parse("12345678-1234-1234-1234-123456789012")
+		assert.Equal(t, expectedUuid, result.Id, "Should have the correct ID")
+	})
+
+	t.Run("server error", func(t *testing.T) {
+		_, invocationContext := setupMockIgnoreContext(t, "Internal Server Error", http.StatusInternalServerError, true)
+
+		var input policyApi.CreatePolicyPayload
+		input.Data.Type = policyApi.CreatePolicyPayloadDataTypePolicy
+
+		_, err := createPolicy(invocationContext, input, testRepoUrl)
+
+		assert.Error(t, err, "Should return an error")
+		assert.Contains(t, err.Error(), "500", "Should contain status code")
+	})
+}
+
+func Test_createPayload(t *testing.T) {
+	expireDate, err := time.Parse(time.DateOnly, "2025-01-01")
+	assert.NoError(t, err)
+	ignoreType := string(policyApi.TemporaryIgnore)
+	reason := "Test reason"
+	findingsId := uuid.New().String()
+
+	payload := createPayload(testRepoUrl, testBranchName, expireDate, ignoreType, reason, findingsId)
+
+	assert.Equal(t, policyApi.CreatePolicyPayloadDataTypePolicy, payload.Data.Type, "Should have correct payload type")
+	assert.Equal(t, ignoreType, string(payload.Data.Attributes.Action.Data.IgnoreType), "Should have correct ignore type")
+	assert.Equal(t, reason, *payload.Data.Attributes.Action.Data.Reason, "Should have correct reason")
+	assert.NotEmpty(t, payload.Data.Attributes.ConditionsGroup.Conditions, "Should have condition")
+	assert.Equal(t, policyApi.Snykassetfindingv1, payload.Data.Attributes.ConditionsGroup.Conditions[0].Field, "Should have correct findings ID")
+	assert.Equal(t, policyApi.Includes, payload.Data.Attributes.ConditionsGroup.Conditions[0].Operator, "Condition operation must be include")
+	assert.Equal(t, findingsId, payload.Data.Attributes.ConditionsGroup.Conditions[0].Value, "Should have correct findings ID")
+	assert.Equal(t, expireDate, *payload.Data.Attributes.Action.Data.Expires, "Should have correct expiration date")
+}
+
+func Test_ignoreCreateWorkflowEntryPoint(t *testing.T) {
+	t.Run("non-interactive mode success", func(t *testing.T) {
+		expectedFindingsId := uuid.New().String()
+		expectedIgnoreType := string(policyApi.TemporaryIgnore)
+		expectedReason := "Test reason"
+		policyId := uuid.New().String()
+		expectedExpirationDate := time.Date(2025, 12, 12, 0, 0, 0, 0, time.UTC)
+		responseMock := fmt.Sprintf(`{
+	  "data": {
+    "id": "%s",
+    "type": "policy",
+    "attributes": {
+      "action": {
+        "data": {
+          "expires": "%s",
+          "ignore_type": "%s",
+          "reason": "%s"
+        }
+      },
+      "action_type": "%s",
+      "conditions_group": {
+        "conditions": [
+          {
+            "field": "%s",
+            "operator": "%s",
+            "value": "%s"
+          }
+        ],
+        "logical_operator": "%s"
+      },
+      "created_by": {
+        "email": "%s"
+      },
+      "review": "%s"
+    }
+}
+}`, policyId, expectedExpirationDate.Format(time.RFC3339), expectedIgnoreType, expectedReason, policyApi.PolicyAttributesActionTypeIgnore, policyApi.Snykassetfindingv1, policyApi.Includes, expectedFindingsId, policyApi.And, expectedUser, policyApi.PolicyReviewPending)
+		_, invocationContext := setupMockIgnoreContext(t, responseMock, http.StatusCreated, true)
+		config := invocationContext.GetConfiguration()
+		config.Set(interactiveKey, false)
+
+		config.Set(findingsIdKey, expectedFindingsId)
+		config.Set(ignoreTypeKey, expectedIgnoreType)
+		config.Set(reasonKey, expectedReason)
+		config.Set(expirationKey, expectedExpirationDate.Format(time.RFC3339))
+		config.Set(remoteRepoUrlKey, testRepoUrl)
+		config.Set(configuration.API_URL, "https://api.snyk.io")
+		config.Set(enrichResponseKey, true)
+
+		result, err := ignoreCreateWorkflowEntryPoint(invocationContext, nil)
+
+		assert.NoError(t, err, "Should not return an error")
+		assert.Greater(t, len(result), 0, "Should return result data")
+		payload := result[0].GetPayload()
+		assert.NotNil(t, payload, "payload should not be nil")
+
+		// Parse the JSON payload
+		var policyResp policyApi.PolicyResponse
+		err = json.Unmarshal(payload.([]byte), &policyResp)
+		assert.NoError(t, err, "Should parse JSON response")
+		assert.Equal(t, policyApi.PolicyResponseTypePolicy, policyResp.Type)
+		assert.Equal(t, policyApi.TemporaryIgnore, policyResp.Attributes.Action.Data.IgnoreType)
+		assert.Equal(t, expectedReason, *policyResp.Attributes.Action.Data.Reason)
+		assert.Equal(t, policyId, policyResp.Id.String())
+		assert.Equal(t, expectedExpirationDate, *policyResp.Attributes.Action.Data.Expires)
+		assert.Equal(t, policyApi.Snykassetfindingv1, policyResp.Attributes.ConditionsGroup.Conditions[0].Field)
+		assert.Equal(t, policyApi.Includes, policyResp.Attributes.ConditionsGroup.Conditions[0].Operator)
+		assert.Equal(t, expectedFindingsId, policyResp.Attributes.ConditionsGroup.Conditions[0].Value)
+	})
+}
