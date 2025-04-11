b8ea5ef591777f99e303f9731a7d3389f070a0be
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2025-04-11 16:09:42 +0200

    fix: ensure to create missing folders for a given output file

--- pkg/local_workflows/output_workflow/findings_model_tools_test.go
+++ pkg/local_workflows/output_workflow/findings_model_tools_test.go
@@ -1,6 +1,7 @@
 package output_workflow
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -59,32 +60,35 @@ func Test_getSarifFileRenderer(t *testing.T) {
 	t.Run("write empty file", func(t *testing.T) {
 		localFindings := getLocalFindingsSkeleton(t, 0)
 		config := configuration.NewWithOpts()
-		config.Set(OUTPUT_CONFIG_KEY_SARIF_FILE, t.TempDir()+"/somefile")
+		config.Set(OUTPUT_CONFIG_KEY_SARIF_FILE, filepath.Join(t.TempDir(), "not-existing", "somefile"))
 		config.Set(OUTPUT_CONFIG_WRITE_EMPTY_FILE, true)
 		renderer, err := getSarifFileRenderer(config, localFindings)
 		assert.NoError(t, err)
 		assert.NotNil(t, renderer)
 		assert.NoError(t, renderer.closer())
+		assert.FileExists(t, config.GetString(OUTPUT_CONFIG_KEY_SARIF_FILE))
 	})
 
 	t.Run("write non empty file", func(t *testing.T) {
 		localFindings := getLocalFindingsSkeleton(t, 1)
 		config := configuration.NewWithOpts()
-		config.Set(OUTPUT_CONFIG_KEY_SARIF_FILE, t.TempDir()+"/somefile")
+		config.Set(OUTPUT_CONFIG_KEY_SARIF_FILE, filepath.Join(t.TempDir(), "not-existing", "somefile"))
 		config.Set(OUTPUT_CONFIG_WRITE_EMPTY_FILE, false)
 		renderer, err := getSarifFileRenderer(config, localFindings)
 		assert.NoError(t, err)
 		assert.NotNil(t, renderer)
 		assert.NoError(t, renderer.closer())
+		assert.FileExists(t, config.GetString(OUTPUT_CONFIG_KEY_SARIF_FILE))
 	})
 
 	t.Run("don't write empty file", func(t *testing.T) {
 		localFindings := getLocalFindingsSkeleton(t, 0)
 		config := configuration.NewWithOpts()
-		config.Set(OUTPUT_CONFIG_KEY_SARIF_FILE, t.TempDir()+"/somefile")
+		config.Set(OUTPUT_CONFIG_KEY_SARIF_FILE, filepath.Join(t.TempDir(), "not-existing", "somefile"))
 		config.Set(OUTPUT_CONFIG_WRITE_EMPTY_FILE, false)
 		renderer, err := getSarifFileRenderer(config, localFindings)
 		assert.NoError(t, err)
 		assert.Nil(t, renderer)
+		assert.NoFileExists(t, config.GetString(OUTPUT_CONFIG_KEY_SARIF_FILE))
 	})
 }
