6527e99cc3753875d7fa6c939bc113d9722f1572
PeterSchafer <101886095+PeterSchafer@users.noreply.github.com>
2025-04-14 14:57:13 +0200

    fix: Ensure to create folders for outputfile (#329)

--- internal/utils/file.go
+++ internal/utils/file.go
@@ -2,9 +2,20 @@ package utils
 
 import (
 	"io/fs"
+	"os"
+	"path/filepath"
 )
 
 const (
 	FILEPERM_755 fs.FileMode = 0755 // Owner=rwx, Group=r-x, Other=r-x
 	FILEPERM_666 fs.FileMode = 0666 // Owner=rw-, Group=rw-, Other=rw-
 )
+
+func CreateFilePath(path string) error {
+	dirPath := filepath.Dir(path)
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		err = os.MkdirAll(dirPath, FILEPERM_755)
+		return err
+	}
+	return nil
+}
