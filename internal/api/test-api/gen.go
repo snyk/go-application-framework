1176504e1b13fe39a3eabe9870c068211e4b02eb
Brian Salomon <brian.salomon@snyk.io>
2025-04-14 20:40:53 -0400

    chore: address PR comments:   - rename files for brevity and convention   - don't cache the temp unified API repo that is pulled for its yaml spec

--- internal/api/test-api/gen.go
+++
@@ -1,3 +0,0 @@
-package unifiedtestapi
-
-//go:generate oapi-codegen -config cfg.yaml unified-test-spec/unified-test-api.yaml
--- scripts/pull-down-unified-test-api-spec.sh
+++ scripts/pull-down-unified-test-api-spec.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-TEST_API_DIR=$(realpath ./internal/api/test-api)
+TEST_API_DIR=$(realpath ./internal/api/testapi)
 
 # Data source
-API_SPEC_PATH=$TEST_API_DIR/unified-test-repo
+API_SPEC_PATH=$(mktemp -d)
 API_SPEC_BRANCH=${API_SPEC_BRANCH:-main}
+trap "rm -rf $API_SPEC_PATH" EXIT
 
 # Outputs
-TEST_API_GENERATED=$TEST_API_DIR/unified-test-spec
+TEST_API_GENERATED=$TEST_API_DIR/2024-10-15
 GENERATE_DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ)
-
 
 echo --------------------------------------------------------
 echo Updating local spec from unified test repo!
@@ -20,10 +20,7 @@ echo Date:   $GENERATE_DATE
 echo --------------------------------------------------------
 
 # Check if the directory exists
-if [[ ! -d "$API_SPEC_PATH" ]]; then
-  # Create the directory if it doesn't exist
-  git clone git@github.com:snyk/test-api-shim.git $API_SPEC_PATH
-fi
+git clone git@github.com:snyk/test-api-shim.git $API_SPEC_PATH
 
 cd $API_SPEC_PATH
 git checkout $API_SPEC_BRANCH
@@ -36,5 +33,5 @@ cd -
 # place OpenAPI build artifacts
 mkdir -p $TEST_API_GENERATED
 
-cp $API_SPEC_PATH/internal/api/closed-beta/versions/2024-10-15/spec.yaml $TEST_API_GENERATED/unified-test-api.yaml
+cp $API_SPEC_PATH/internal/api/closed-beta/versions/2024-10-15/spec.yaml $TEST_API_GENERATED/spec.yaml
 echo $GENERATE_DATE $API_SPEC_BRANCH $API_COMMIT > $TEST_API_GENERATED/generated.txt
