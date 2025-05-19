ffcd6208e320f153a7fee73adf4b5b4df44e3b62
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2025-05-19 16:56:47 +0200

    chore: add helper to access feature flags

--- pkg/local_workflows/config_utils/feature_flag.go
+++ pkg/local_workflows/config_utils/feature_flag.go
@@ -1,6 +1,8 @@
 package config_utils
 
 import (
+	"net/http"
+
 	"github.com/snyk/go-application-framework/internal/api"
 	"github.com/snyk/go-application-framework/pkg/configuration"
 	"github.com/snyk/go-application-framework/pkg/workflow"
@@ -12,19 +14,19 @@ func AddFeatureFlagToConfig(engine workflow.Engine, configKey string, featureFla
 	callback := func(existingValue interface{}) (interface{}, error) {
 		if existingValue == nil {
 			httpClient := engine.GetNetworkAccess().GetHttpClient()
-			logger := engine.GetLogger()
-			url := config.GetString(configuration.API_URL)
-			org := config.GetString(configuration.ORGANIZATION)
-			apiClient := api.NewApi(url, httpClient)
-			result, err := apiClient.GetFeatureFlag(featureFlagName, org)
-			if err != nil {
-				logger.Printf("Failed to determine feature flag \"%s\" for org \"%s\": %s", featureFlagName, org, err)
-			}
-			return result, nil
+			return GetFeatureFlagValue(featureFlagName, config, httpClient)
 		} else {
 			return existingValue, nil
 		}
 	}
 
 	config.AddDefaultValue(configKey, callback)
+}
+
+func GetFeatureFlagValue(featureFlagName string, config configuration.Configuration, httpClient *http.Client) (bool, error) {
+	url := config.GetString(configuration.API_URL)
+	org := config.GetString(configuration.ORGANIZATION)
+	apiClient := api.NewApi(url, httpClient)
+	result, err := apiClient.GetFeatureFlag(featureFlagName, org)
+	return result, err
 }
