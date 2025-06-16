9b2446fa969d5bbd360d2b39982aec8cca2031da
George Shank <george.shank@snyk.io>
2025-06-16 12:43:57 -0400

    feat(snykschema): add snyk schema content type for gaf pipeline

--- pkg/local_workflows/content_type/constants.go
+++ pkg/local_workflows/content_type/constants.go
@@ -1,7 +1,8 @@
 package content_type
 
 const (
-	TEST_SUMMARY        = "application/json; schema=test-summary"
-	LOCAL_FINDING_MODEL = "application/json; schema=local-finding-summary"
-	SARIF_JSON          = "application/sarif+json"
+	TEST_SUMMARY             = "application/json; schema=test-summary"
+	LOCAL_FINDING_MODEL      = "application/json; schema=local-finding-summary"
+	SARIF_JSON               = "application/sarif+json"
+	SNYK_SCHEMA_TEST_SUMMARY = "application/json; schema=snyk-schema-test-summary"
 )
