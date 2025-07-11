9bbd4721a10b862633da384d3d2b42b20b532566
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2025-07-11 13:49:39 +0200

    chore: add comment

--- pkg/local_workflows/output_workflow/newlinewritecloser.go
+++ pkg/local_workflows/output_workflow/newlinewritecloser.go
@@ -14,6 +14,7 @@ func (wc *newLineCloser) Write(p []byte) (n int, err error) {
 }
 
 func (wc *newLineCloser) Close() error {
+	// template based renders had an artifact "%" at the end of the content which disappears when adding a newline
 	_, err := fmt.Fprintln(wc.writer, "")
 	return err
 }
