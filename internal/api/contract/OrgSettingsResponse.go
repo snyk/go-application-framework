5db2d8a6c010a604cd33aacbda01e9e5f2be9bd7
CatalinSnyk <catalin.iuga@snyk.io>
2025-08-06 07:45:55 +0300

    chore: add missing field from org setting model

--- internal/api/contract/OrgSettingsResponse.go
+++ internal/api/contract/OrgSettingsResponse.go
@@ -6,6 +6,11 @@ type OrgIgnoreSettings struct {
 	ApprovalWorkflowEnabled bool `json:"approvalWorkflowEnabled,omitempty"`
 }
 
+type OrgRequestAccessSettings struct {
+	Enabled bool `json:"enabled,omitempty"`
+}
+
 type OrgSettingsResponse struct {
-	Ignores OrgIgnoreSettings `json:"ignores"`
+	Ignores       OrgIgnoreSettings        `json:"ignores"`
+	RequestAccess OrgRequestAccessSettings `json:"requestAccess"`
 }
