66d2080267ab696d848946884b7ddc7f9e536a2d
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-08-19 12:20:36 +0200

    fix: clear credentials before auth (#406)

--- pkg/local_workflows/auth_workflow.go
+++ pkg/local_workflows/auth_workflow.go
@@ -60,6 +60,8 @@ func authEntryPoint(invocationCtx workflow.InvocationContext, _ []workflow.Data)
 	logger := invocationCtx.GetEnhancedLogger()
 	engine := invocationCtx.GetEngine()
 
+	config.ClearCache()
+
 	httpClient := invocationCtx.GetNetworkAccess().GetUnauthorizedHttpClient()
 	authenticator := auth.NewOAuth2AuthenticatorWithOpts(
 		config,
@@ -107,10 +109,13 @@ func entryPointDI(invocationCtx workflow.InvocationContext, logger *zerolog.Logg
 	logger.Printf("Authentication Type: %s", authType)
 	analytics.AddExtensionStringValue(authTypeParameter, authType)
 
-	if strings.EqualFold(authType, auth.AUTH_TYPE_OAUTH) { // OAUTH flow
-		logger.Printf("Unset legacy token key %q from config", configuration.AUTHENTICATION_TOKEN)
-		config.Unset(configuration.AUTHENTICATION_TOKEN)
+	existingSnykToken := config.GetString(configuration.AUTHENTICATION_TOKEN)
+	// always attempt to clear existing tokens before triggering auth
+	logger.Print("Unset existing auth keys")
+	config.Unset(configuration.AUTHENTICATION_TOKEN)
+	config.Unset(auth.CONFIG_KEY_OAUTH_TOKEN)
 
+	if strings.EqualFold(authType, auth.AUTH_TYPE_OAUTH) { // OAUTH flow
 		headless := config.GetBool(headlessFlag)
 		logger.Printf("Headless: %v", headless)
 
@@ -125,25 +130,19 @@ func entryPointDI(invocationCtx workflow.InvocationContext, logger *zerolog.Logg
 		}
 	} else if strings.EqualFold(authType, auth.AUTH_TYPE_PAT) { // PAT flow
 		engine.GetConfiguration().PersistInStorage(auth.CONFIG_KEY_TOKEN)
-
-		oldToken := config.GetString(configuration.AUTHENTICATION_TOKEN)
 		pat := config.GetString(ConfigurationNewAuthenticationToken)
-
-		logger.Print("Unset existing auth keys from config")
-		config.Unset(auth.CONFIG_KEY_OAUTH_TOKEN)
-		config.Unset(configuration.AUTHENTICATION_TOKEN)
 
 		logger.Print("Validating pat")
 		whoamiConfig := config.Clone()
-		// we don't want to use the cache here, so this is a workaround
 		whoamiConfig.ClearCache()
+		// we don't want to use the cache here, so this is a workaround
 		whoamiConfig.Set(configuration.FLAG_EXPERIMENTAL, true)
 		whoamiConfig.Set(configuration.AUTHENTICATION_TOKEN, pat)
 		_, whoamiErr := engine.InvokeWithConfig(workflow.NewWorkflowIdentifier("whoami"), whoamiConfig)
 		if whoamiErr != nil {
 			// reset config file
-			if len(oldToken) > 0 {
-				config.Set(auth.CONFIG_KEY_TOKEN, oldToken)
+			if len(existingSnykToken) > 0 {
+				config.Set(auth.CONFIG_KEY_TOKEN, existingSnykToken)
 			}
 			return whoamiErr
 		}
@@ -158,9 +157,6 @@ func entryPointDI(invocationCtx workflow.InvocationContext, logger *zerolog.Logg
 			logger.Debug().Err(err).Msg("Failed to output authenticated message")
 		}
 	} else { // LEGACY flow
-		logger.Printf("Unset oauth key %q from config", auth.CONFIG_KEY_OAUTH_TOKEN)
-		config.Unset(auth.CONFIG_KEY_OAUTH_TOKEN)
-
 		config.Set(configuration.RAW_CMD_ARGS, os.Args[1:])
 		config.Set(configuration.WORKFLOW_USE_STDIO, true)
 		config.Set(configuration.AUTHENTICATION_TOKEN, "") // clear token to avoid using it during authentication
