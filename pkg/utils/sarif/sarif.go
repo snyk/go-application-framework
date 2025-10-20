d711d27fdd396762e72bdb68c27cca17e15d0d1e
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2025-10-20 18:51:06 +0200

    feat: ufm sarif rendering

--- pkg/utils/sarif/sarif.go
+++ pkg/utils/sarif/sarif.go
@@ -1,8 +1,16 @@
 package sarif
 
 import (
-	"github.com/snyk/code-client-go/sarif"
+	"context"
+	"fmt"
+	"maps"
+	"slices"
 
+	"github.com/snyk/code-client-go/sarif"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
+	"github.com/snyk/go-application-framework/pkg/apiclients/testapi"
 	"github.com/snyk/go-application-framework/pkg/local_workflows/json_schemas"
 )
 
@@ -132,5 +140,207 @@ func ConvertTypeToDriverName(s string) string {
 		return "Snyk IaC"
 	default:
 		return "Snyk Open Source"
+	}
+}
+
+// GetRulesFromTestResult extracts SARIF rules from test results for a specific finding type.
+// Based on the TypeScript implementation in open-source-sarif-output.ts
+func GetRulesFromTestResult(result testapi.TestResult, t testapi.FindingType) []map[string]interface{} {
+	sarifRules := map[string]map[string]interface{}{}
+
+	findings, _, err := result.Findings(context.Background())
+	if err != nil {
+		return []map[string]interface{}{}
+	}
+
+	for _, finding := range findings {
+		if finding.Attributes.FindingType == t {
+			for _, problem := range finding.Attributes.Problems {
+				if discriminator, err := problem.Discriminator(); err == nil && discriminator != "snyk_vuln" {
+					continue
+				}
+
+				vulnProblem, err := problem.AsSnykVulnProblem()
+				if err != nil {
+					continue
+				}
+
+				if _, ok := sarifRules[vulnProblem.Id]; !ok {
+					// Build shortDescription: "${upperFirst(severity)} severity - ${title} vulnerability in ${packageName}"
+					shortDesc := fmt.Sprintf("%s severity - %s vulnerability in %s",
+						cases.Title(language.English).String(string(vulnProblem.Severity)),
+						finding.Attributes.Title,
+						vulnProblem.PackageName)
+
+					// Build fullDescription: cves ? "(${cves}) ${name}@${version}" : "${name}@${version}"
+					fullDesc := fmt.Sprintf("%s@%s", vulnProblem.PackageName, vulnProblem.PackageVersion)
+					// TODO: Add CVE identifiers when available from References
+
+					// Build help markdown (simplified version - can be enhanced)
+					helpMarkdown := fmt.Sprintf("* Vulnerable module: %s\n* Introduced through: %s@%s\n\n%s",
+						vulnProblem.PackageName,
+						vulnProblem.PackageName,
+						vulnProblem.PackageVersion,
+						finding.Attributes.Description)
+
+					// Build properties
+					properties := map[string]interface{}{
+						"cvssv3_baseScore":  vulnProblem.CvssBaseScore,                      // AWS
+						"security-severity": fmt.Sprintf("%.1f", vulnProblem.CvssBaseScore), // GitHub
+						"tags":              []string{"security"},                           // Can add CWEs and package manager
+					}
+
+					sarifRules[vulnProblem.Id] = map[string]interface{}{
+						"id":               vulnProblem.Id,
+						"shortDescription": shortDesc,
+						"fullDescription":  fullDesc,
+						"help_text":        "",
+						"help_markdown":    helpMarkdown,
+						"properties":       properties,
+					}
+				}
+			}
+		}
+	}
+	return slices.Collect(maps.Values(sarifRules))
+}
+
+// GetResultsFromTestResult extracts SARIF results from test results for a specific finding type.
+// Currently supports SCA findings, can be extended for SAST later.
+func GetResultsFromTestResult(result testapi.TestResult, t testapi.FindingType) []map[string]interface{} {
+	var results []map[string]interface{}
+
+	findings, _, err := result.Findings(context.Background())
+	if err != nil {
+		return results
+	}
+
+	for _, finding := range findings {
+		if finding.Attributes.FindingType != t {
+			continue
+		}
+
+		// Handle SCA findings
+		if t == testapi.FindingTypeSca {
+			for _, problem := range finding.Attributes.Problems {
+				if discriminator, err := problem.Discriminator(); err == nil && discriminator != "snyk_vuln" {
+					continue
+				}
+
+				vulnProblem, err := problem.AsSnykVulnProblem()
+				if err != nil {
+					continue
+				}
+
+				// Build message
+				message := map[string]interface{}{
+					"text": fmt.Sprintf("This file introduces a vulnerable %s package with a %s severity vulnerability.",
+						vulnProblem.PackageName,
+						vulnProblem.Severity),
+				}
+
+				// Build location - use first location if available, otherwise default
+				location := buildScaLocation(finding, vulnProblem)
+
+				// Build result
+				sarifResult := map[string]interface{}{
+					"ruleId":    vulnProblem.Id,
+					"level":     SeverityToSarifLevel(string(vulnProblem.Severity)),
+					"message":   message,
+					"locations": []interface{}{location},
+				}
+
+				// Add fixes if available
+				if vulnProblem.IsFixable && len(vulnProblem.InitiallyFixedInVersions) > 0 {
+					fixes := buildScaFixes(finding, vulnProblem)
+					if fixes != nil {
+						sarifResult["fixes"] = fixes
+					}
+				}
+
+				results = append(results, sarifResult)
+			}
+		}
+		// TODO: Add SAST handling here later
+		// else if t == testapi.FindingTypeSast { ... }
+	}
+
+	return results
+}
+
+func buildScaLocation(finding testapi.FindingData, vulnProblem testapi.SnykVulnProblem) map[string]interface{} {
+	// Default to line 1 for manifest files
+	uri := "package.json" // Default, should be determined from locations
+	startLine := 1
+
+	// Try to extract actual file path from locations
+	if len(finding.Attributes.Locations) > 0 {
+		loc := finding.Attributes.Locations[0]
+		sourceLoc, err := loc.AsSourceLocation()
+		if err == nil && sourceLoc.FilePath != "" {
+			uri = sourceLoc.FilePath
+			startLine = sourceLoc.FromLine
+		}
+	}
+
+	return map[string]interface{}{
+		"physicalLocation": map[string]interface{}{
+			"artifactLocation": map[string]interface{}{
+				"uri": uri,
+			},
+			"region": map[string]interface{}{
+				"startLine": startLine,
+			},
+		},
+		"logicalLocations": []interface{}{
+			map[string]interface{}{
+				"fullyQualifiedName": fmt.Sprintf("%s@%s", vulnProblem.PackageName, vulnProblem.PackageVersion),
+			},
+		},
+	}
+}
+
+func buildScaFixes(finding testapi.FindingData, vulnProblem testapi.SnykVulnProblem) []interface{} {
+	if len(vulnProblem.InitiallyFixedInVersions) == 0 {
+		return nil
+	}
+
+	fixedVersion := vulnProblem.InitiallyFixedInVersions[0]
+	uri := "package.json"
+	startLine := 1
+
+	// Try to extract actual file path from locations
+	if len(finding.Attributes.Locations) > 0 {
+		loc := finding.Attributes.Locations[0]
+		sourceLoc, err := loc.AsSourceLocation()
+		if err == nil && sourceLoc.FilePath != "" {
+			uri = sourceLoc.FilePath
+			startLine = sourceLoc.FromLine
+		}
+	}
+
+	return []interface{}{
+		map[string]interface{}{
+			"description": map[string]interface{}{
+				"text": fmt.Sprintf("Upgrade to %s", fixedVersion),
+			},
+			"artifactChanges": []interface{}{
+				map[string]interface{}{
+					"artifactLocation": map[string]interface{}{
+						"uri": uri,
+					},
+					"replacements": []interface{}{
+						map[string]interface{}{
+							"deletedRegion": map[string]interface{}{
+								"startLine": startLine,
+							},
+							"insertedContent": map[string]interface{}{
+								"text": fixedVersion,
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 }
