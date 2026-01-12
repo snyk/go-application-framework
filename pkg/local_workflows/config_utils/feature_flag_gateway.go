625f91300e493c63565fda70d9fb6f90af3793ba
Tom Brewster <brew42@hotmail.com>
2026-01-12 14:05:37 +0000

    chore: add feature flag gateway client and wrapper [CSENG-68]
    chore: bump crypto version [CSENG-68]
    
    chore: bump crypto version [CSENG-68]
    
    chore: update feature flag gw warpper to write to config [CSENG-68]
    
    fix: update deps to fix CVE-2025-47913 (#521)
    
    chore: update for pr comments

---
+++ pkg/local_workflows/config_utils/feature_flag_gateway.go
@@ -0,0 +1,67 @@
+package config_utils
+
+import (
+	"github.com/google/uuid"
+	featureflaggateway "github.com/snyk/go-application-framework/pkg/apiclients/feature_flag_gateway"
+	v20241015 "github.com/snyk/go-application-framework/pkg/apiclients/feature_flag_gateway/2024-10-15"
+	"github.com/snyk/go-application-framework/pkg/configuration"
+	"github.com/snyk/go-application-framework/pkg/workflow"
+)
+
+var evaluateFlags = featureflaggateway.EvaluateFlags
+
+func AddFeatureFlagGatewayToConfig(engine workflow.Engine, configKey string, featureFlagName string) {
+	config := engine.GetConfiguration()
+	err := config.AddKeyDependency(configKey, configuration.ORGANIZATION)
+	if err != nil {
+		engine.GetLogger().Err(err).Msgf("Failed to add dependency for %s", configKey)
+	}
+	orgID := config.GetString(configuration.ORGANIZATION)
+
+	callback := func(c configuration.Configuration, existingValue any) (any, error) {
+		if existingValue != nil {
+			return existingValue, nil
+		}
+
+		enabled := isFeatureEnabled(
+			c,
+			engine,
+			orgID,
+			featureFlagName,
+		)
+		return enabled, nil
+	}
+
+	config.AddDefaultValue(configKey, callback)
+}
+
+func isFeatureEnabled(
+	config configuration.Configuration,
+	engine workflow.Engine,
+	orgID string,
+	flag string,
+) bool {
+	orgUUID, err := uuid.Parse(orgID)
+	if err != nil {
+		return false
+	}
+
+	resp, err := evaluateFlags(config, engine, []string{flag}, orgUUID)
+	if err != nil || !validEvaluateFlagsResponse(resp) {
+		return false
+	}
+
+	evaluations := resp.ApplicationvndApiJSON200.Data.Attributes.Evaluations
+	for _, e := range evaluations {
+		if e.Key == flag {
+			return *e.Value
+		}
+	}
+	return false
+}
+
+func validEvaluateFlagsResponse(resp *v20241015.ListFeatureFlagsResponse) bool {
+	return resp != nil &&
+		resp.ApplicationvndApiJSON200 != nil &&
+		resp.ApplicationvndApiJSON200.Data != nil
+}
