16bd372921c2f275d01a73b9d4151c7e075e5153
Paul Rosca <paul.rosca@snyk.io>
2026-02-04 18:01:08 +0200

    feat(fileupload): remove custom filters

--- pkg/apiclients/fileupload/filter.go
+++ pkg/apiclients/fileupload/filter.go
@@ -2,6 +2,8 @@ package fileupload
 
 import "os"
 
+// fileToFilter represents the metadata about a file that a Filter function gets
+// in order to decide if the file should be filtered or not.
 type fileToFilter struct {
 	Path string
 	Stat os.FileInfo
@@ -14,4 +16,8 @@ type FilteredFile struct {
 	Reason error
 }
 
+// filter is the type definition for functions which are used to
+// filter files before uploading.
+// The function will get metadata about files in the form of FileToFilter
+// and return a FilteredFile object if the file should be filtered, or nil otherwise.
 type filter func(fileToFilter) *FilteredFile
