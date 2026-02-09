20f85bced5987fe36273627e45e71d27f2a7af4d
CatalinSnyk <catalin.iuga@snyk.io>
2026-02-09 12:22:04 +0200

    chore: add multi project scan test for human readable

--- internal/presenters/styles.go
+++ internal/presenters/styles.go
@@ -17,9 +17,10 @@ func renderBold(str string) string {
 
 func renderInSeverityColor(severity string, str string) string {
 	severityToColor := map[string]lipgloss.TerminalColor{
-		"LOW":    lipgloss.NoColor{},
-		"MEDIUM": lipgloss.AdaptiveColor{Light: "11", Dark: "3"},
-		"HIGH":   lipgloss.AdaptiveColor{Light: "9", Dark: "1"},
+		"LOW":      lipgloss.NoColor{},
+		"MEDIUM":   lipgloss.AdaptiveColor{Light: "11", Dark: "3"},
+		"HIGH":     lipgloss.AdaptiveColor{Light: "9", Dark: "1"},
+		"CRITICAL": lipgloss.AdaptiveColor{Light: "13", Dark: "5"},
 	}
 	severityStyle := lipgloss.NewStyle().Foreground(severityToColor[strings.ToUpper(severity)])
 	return severityStyle.Render(str)
