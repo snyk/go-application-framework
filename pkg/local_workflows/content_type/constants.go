8229862e2dc42126c19750143441c672441dbbbd
JSON <jason.luong@snyk.io>
2026-02-17 11:29:09 +0000

    chore: rename const for legacy-cli content-type (#550)

--- pkg/local_workflows/content_type/constants.go
+++ pkg/local_workflows/content_type/constants.go
@@ -3,7 +3,7 @@ package content_type
 const (
 	TEST_SUMMARY        = "application/json; schema=test-summary"
 	LOCAL_FINDING_MODEL = "application/json; schema=local-finding-summary"
-	LEGACY_CLI_STDOUT   = "application/json; schema=legacy-cli"
+	LEGACY_CLI          = "application/json; schema=legacy-cli"
 	SARIF_JSON          = "application/sarif+json"
 	UFM_RESULT          = "application/ufm.result"
 )
