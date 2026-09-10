02e6e364a050daaaf0c23a3a0b095e2c2eba17a9
octavian-snyk <octavian.comanescu@snyk.io>
2026-09-10 11:56:24 +0300

    feat: add TOON flags (#765)

--- pkg/instrumentation/categories.go
+++ pkg/instrumentation/categories.go
@@ -193,6 +193,8 @@ var known_flags = []string{
 	"tfc-endpoint",
 	"tfc-token",
 	"to",
+	"toon",
+	"toon-file-output",
 	"traverse-node-modules",
 	"trust-policies",
 	"unmanaged",
--- pkg/local_workflows/output_workflow_test.go
+++ pkg/local_workflows/output_workflow_test.go
@@ -145,6 +145,12 @@ func Test_Output_InitOutputWorkflow(t *testing.T) {
 
 	htmlFileOutput := config.Get("html-file-output")
 	assert.Equal(t, "", htmlFileOutput)
+
+	toon := config.Get("toon")
+	assert.Equal(t, false, toon)
+
+	toonFileOutput := config.Get("toon-file-output")
+	assert.Equal(t, "", toonFileOutput)
 }
 
 type testOutputDestination struct {
