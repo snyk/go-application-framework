2da1f87e42a75dcaf8d58299f0023dedd5a5eb9e
Roberto López López <roberto.lopez-lopez@snyk.io>
2026-09-18 16:44:36 +0200

    clean: issues.go, lint

--- pkg/apiclients/testapi/issues.go
+++ pkg/apiclients/testapi/issues.go
@@ -722,7 +722,7 @@ func (b *issueBuilder) processProblems(finding *FindingData) {
 			b.processCveProblem(&problem)
 		case "cwe":
 			b.processCweProblem(&problem)
-		case "secret", "snyk_secrets_rule":
+		case "secret":
 			b.processSecretsRuleProblem(&problem)
 		}
 
